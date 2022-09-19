55353987bea9410139649783b3ee36fb4c7c8645
Dylan Palmboom <nofxboy1234@gmail.com>
2022-09-19 12:13:03 +0200

    Dynamically generate bundle names based on entry point names

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import _ from 'lodash';
+import printMe from './print';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // Lodash, now imported by this scripot
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
   return element;
 }
 
