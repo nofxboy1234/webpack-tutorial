755dd114eb52adfd7f205a141e2e7d959521fadf
Dylan Palmboom <nofxboy1234@gmail.com>
2022-09-19 16:41:56 +0200

    Use webpack-dev-middleware with express

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function component() {
   const btn = document.createElement('button');
 
   // Lodash, now imported by this scripot
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
 
   btn.innerHTML = 'Click me and check the console!!!';
   btn.onclick = printMe;
